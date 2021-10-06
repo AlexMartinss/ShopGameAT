d7cba0648febd78d840689272f71b8929d6c12a0
AlexMartins <thealexmartins@gmail.com>
2021-10-06 16:04:11 +1100

    Created Movement and Pickup Script

---
+++ ShopGameAT/Assets/Scripts/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject Base;
+    public GameObject Package;
+
+
+    void Start()
+    {
+        Base.gameObject.SetActive(true);
+        Package.gameObject.SetActive(false);
+
+    }
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Package")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Base.gameObject.SetActive(false);
+                Package.gameObject.SetActive(true);
+
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
